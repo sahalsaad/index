89beaecf86b882e997d8e55828af17a2bdf97908
sahalsaad <caalshift@gmail.com>
2021-08-19 12:08:42 +0800

    Update standard version

--- src/component/Bio.jsx
+++ src/component/Bio.jsx
@@ -10,10 +10,10 @@ line-height: 1.5em;
 
 const Bio = () => (
   <BioParagraph>
-        I'm a full stack developer. <br />
-        I have been dabbling in web development for the past {moment(['2015', '01', '01']).fromNow(true)}, often
-        experimenting and learning new things. My passion drives me to stay
-        updated with the latest trends in the field of cyber, web and information technology.
+    I'm a full stack developer. <br />
+    I have been dabbling in web development for the past {moment(['2015', '01', '01']).fromNow(true)}, often
+    experimenting and learning new things. My passion drives me to stay
+    updated with the latest trends in the field of cyber, web and information technology.
   </BioParagraph>
 )
 
