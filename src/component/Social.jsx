61a96a9823932e36b402ec1feba64a812c42c489
Sahal Saad <caalshift@gmail.com>
2023-12-08 07:22:40 +0800

    Remove unused variable

--- src/component/Social.jsx
+++ src/component/Social.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLinkedin, faBitbucket, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 const SocailContainer = styled.div`
 padding-top: 0px;
